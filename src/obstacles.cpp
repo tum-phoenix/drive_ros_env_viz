447026f21da4330f2d11f9a508ca53094aa450d3
Unknown <dev-faha@t-online.de>
2018-06-14 16:52:20 +0200

    add box marker

--- src/obstacles.cpp
+++ src/obstacles.cpp
@@ -23,7 +23,7 @@ void cb(const drive_ros_msgs::ObstacleArrayConstPtr& msg)
     for(auto it = msg->obstacles.begin(); it != msg->obstacles.end(); it++)
     {
 
-        // determine color of points
+        // determine color
         float r=0.0, g=0.0, b=0.0;
 
         switch (it->obstacle_type) {
@@ -71,7 +71,9 @@ void cb(const drive_ros_msgs::ObstacleArrayConstPtr& msg)
         ct.id = id;
         ct.type = visualization_msgs::Marker::SPHERE;
         ct.action = visualization_msgs::Marker::ADD;
-        ct.pose.position = it->centroid;
+        ct.pose.position.x = it->centroid.x;
+        ct.pose.position.y = it->centroid.y;
+        ct.pose.position.z = it->centroid.z;
         ct.scale.x = 0.05;
         ct.scale.y = 0.05;
         ct.scale.z = 0.05;
@@ -81,6 +83,27 @@ void cb(const drive_ros_msgs::ObstacleArrayConstPtr& msg)
         ct.color.b = b;
         id++;
         obs.markers.push_back(ct);
+
+        // draw box
+        visualization_msgs::Marker box;
+        box.header.stamp = it->header.stamp;
+        box.header.frame_id = it->header.frame_id;
+        box.ns = ns;
+        box.id = id;
+        box.type = visualization_msgs::Marker::CUBE;
+        box.action = visualization_msgs::Marker::ADD;
+        box.pose.position.x = it->centroid.x;
+        box.pose.position.y = it->centroid.y;
+        box.pose.position.z = it->centroid.z;
+        box.scale.x = std::max(float(0.01), std::cos(it->yaw)*it->length + std::sin(it->yaw)*it->width);
+        box.scale.y = std::max(float(0.01), std::sin(it->yaw)*it->length + std::cos(it->yaw)*it->width);
+        box.scale.z = std::max(it->height, float(0.1));
+        box.color.a = it->trust;
+        box.color.r = r;
+        box.color.g = g;
+        box.color.b = b;
+        id++;
+        obs.markers.push_back(box);
     }
     pub_marker.publish(obs);
 }
